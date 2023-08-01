23a6df5dc427d7933824d7c27f9c502fd2a780b3
rugema3 <rugema61@gmail.com>
2023-08-01 22:34:11 +0200

    call me maybe

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+def match_phone_number(input)
+  matches = input.match(/^\d{10}$/)
+  matches ? matches[0] : ""
+end
+
+# Get the first command-line argument
+input = ARGV[0]
+
+# Call the custom method to find and print the matched result
+puts match_phone_number(input) + "$"
+
