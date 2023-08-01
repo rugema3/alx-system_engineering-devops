ba42d28fe95dd6bf92cfe934ea51e6d5643a52ba
rugema3 <rugema61@gmail.com>
2023-08-01 22:38:55 +0200

    caps only

---
+++ 0x06-regular_expressions/OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+def match_capital_letters(input)
+  matches = input.scan(/[A-Z]/).join
+  matches.empty? ? "" : matches + "SYSADMIN"
+end
+
+# Get the first command-line argument
+input = ARGV[0]
+
+# Call the custom method to find and print the matched result
+puts match_capital_letters(input)
