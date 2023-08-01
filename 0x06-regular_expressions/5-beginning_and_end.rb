415b23b84055efcba5895de67ff97dc9d9b2208e
rugema3 <rugema61@gmail.com>
2023-08-01 22:24:25 +0200

    updates

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 
 def match_beginning_and_end(arg)
-  matches = arg.match(/^h.n$/)
+  matches = input.match(/^h.n$/)
   matches ? matches[0] : ""
 end
 
 # Get the first command-line argument
-arg = ARGV[0]
+input = ARGV[0]
 
 # Call the custom method to find and print the matched result
-puts match_beginning_and_end(arg) + "$"
+puts match_beginning_and_end(input) + "$"
