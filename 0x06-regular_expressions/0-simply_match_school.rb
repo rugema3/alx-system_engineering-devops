19a76b37bc7b97c2d362a7f5afb993a3f85a1c74
rugema3 <rugema61@gmail.com>
2023-08-01 21:44:16 +0200

    usinga  different approach

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,15 @@
 #!/usr/bin/env ruby
 
-puts ARGV[0].gsub(/School/, 'Match: School')
+# Get the first command-line argument
+input = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /School/
+
+# Match the pattern against the input and print the result
+match_result = input.scan(pattern).join
+
+# Print the matched result
+puts match_result + "$"
+
 
