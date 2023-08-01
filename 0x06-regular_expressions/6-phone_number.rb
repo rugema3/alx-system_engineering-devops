2b9668e0dc8609d6bf3ce062ce7d7edad952ec99
rugema3 <rugema61@gmail.com>
2023-08-01 22:35:52 +0200

    call me maybe

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 def match_phone_number(input)
-  matches = input.match(/^\d{10}$/)
+  matches = input.match(/^\d{10,10}$/)
   matches ? matches[0] : ""
 end
 
@@ -9,5 +9,5 @@ end
 input = ARGV[0]
 
 # Call the custom method to find and print the matched result
-puts match_phone_number(input) + "$"
+puts match_phone_number(input)
 
