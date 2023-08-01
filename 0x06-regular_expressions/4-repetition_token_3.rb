fc4108a3cf221d975ccab1b8517bc9a1b35061b1
rugema3 <rugema61@gmail.com>
2023-08-01 22:18:14 +0200

    Repetition Token #3

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+def find_hbtn_matches(input)
+  matches = input.scan(/hbt*n/)
+  matches.join
+end
+
+# Get the first command-line argument
+input = ARGV[0]
+
+# Call the custom method to find and print the matches
+puts find_hbtn_matches(input)
