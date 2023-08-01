128efe90bc33abded81e5e2d44fa12affb4aa93e
rugema3 <rugema61@gmail.com>
2023-08-01 21:52:31 +0200

    updates

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
-ARGV.each do |arg|
-  match_result = arg.match(/School/)
-  puts match_result ? "Match found: #{match_result}" : "No match found."
+
+def find_school_occurrences(input)
+  matches = input.scan(/School/)
+  matches.join
 end
 
+# Get the first command-line argument
+input = ARGV[0]
+
+# Call the custom method to find and print the occurrences of "School"
+puts find_school_occurrences(input)
