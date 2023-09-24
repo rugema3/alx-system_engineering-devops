7455690fabefcfb91a26621b8310cd6b4dcfee02
rugema3 <rugema61@gmail.com>
2023-09-24 23:57:19 +0200

    modifications

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
-exec { 'configure_ssh_client':
-  command => 'echo -e "IdentityFile ~/.ssh/school\nPasswordAuthentication no" | tee -a /etc/ssh/ssh_config',
-  unless  => 'grep -qE "^\s*IdentityFile\s+~/.ssh/school" /etc/ssh/ssh_config && grep -qE "^\s*PasswordAuthentication\s+no" /etc/ssh/ssh_config',
-  require => File['/etc/ssh/ssh_config'],
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => 0,
 }
 
