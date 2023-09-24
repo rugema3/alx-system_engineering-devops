b0fa626c95a0ea8673c35b22edeb1a75d2f6004a
rugema3 <rugema61@gmail.com>
2023-09-25 00:33:12 +0200

    modifications to ssh file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,8 @@
+# Modification to ssh config file
+
 exec { 'echo':
   path    => 'usr/bin:/bin',
   command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-  returns => 0,
+  returns => [0,1],
 }
 
