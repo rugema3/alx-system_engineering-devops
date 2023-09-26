98b5badf258f876eb7490aeff0c387ba9a157aa6
rugema3 <rugema61@gmail.com>
2023-09-26 19:58:15 +0200

    updates

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,45 +1,8 @@
-# Define a class for Nginx installation and configuration
-class nginx_server {
+# Installs Nginx server and configures redirection
 
-  # Install Nginx package
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Configure Nginx to listen on port 80
-  file { '/etc/nginx/sites-available/default':
-    ensure => present,
-    source => 'puppet:///modules/nginx_server/default',
-    notify => Service['nginx'],
-  }
-
-  # Create the custom index file with "Hello World!"
-  file { '/var/www/html/index.html':
-    ensure  => present,
-    content => 'Hello World!',
-    notify  => Service['nginx'],
-  }
-
-  # Enable the default site by creating a symbolic link
-  file { '/etc/nginx/sites-enabled/default':
-    ensure => link,
-    target => '/etc/nginx/sites-available/default',
-    notify => Service['nginx'],
-  }
-
-  # Define a redirect resource
-  nginx::resource::location { 'redirect_me':
-    location      => '/redirect_me',
-    rewrite_rule  => 'https://www.youtube.com/watch?v=QH2-TGUlwu4/ permanent',
-  }
-
-  # Ensure Nginx service is running and enabled
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-  }
+exec { 'install_nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  unless   => 'dpkg -l | grep -qw nginx', # Ensures Nginx is not reinstalled
 }
-
-# Include the Nginx class
-include nginx_server
 
