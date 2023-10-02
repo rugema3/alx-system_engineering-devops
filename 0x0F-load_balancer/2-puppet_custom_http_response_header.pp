72cf41ff64f50188438d2c9db10f29517d7722bc
rugema3 <rugema61@gmail.com>
2023-10-02 23:09:03 +0200

    modifications

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,15 +1,21 @@
-# 2-puppet_custom_http_response_header.pp
-
 # Install Nginx
-class { 'nginx': }
+exec { 'update':
+  command => 'sudo apt-get -y update',
+  path    => ['/usr/bin', '/usr/sbin'],
+}
 
-# Define the custom Nginx configuration
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update'],
+}
+
+# Create a custom Nginx configuration file
 file { '/etc/nginx/sites-available/custom_header':
   ensure  => 'file',
   content => "server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    add_header X-Served-By $hostname;
+    add_header X-Served-By \$hostname;
     root   /var/www/html;
     index  index.html index.htm;
 
@@ -23,28 +29,38 @@ file { '/etc/nginx/sites-available/custom_header':
       internal;
     }
 }",
+  require => Package['nginx'],
 }
 
 # Create a symbolic link to enable the custom configuration
 file { '/etc/nginx/sites-enabled/custom_header':
   ensure  => 'link',
   target  => '/etc/nginx/sites-available/custom_header',
+  require => File['/etc/nginx/sites-available/custom_header'],
 }
 
 # Create the default HTML file with your content
 file { '/var/www/html/index.nginx-debian.html':
   ensure  => 'file',
   content => 'Hello World!',
+  require => Package['nginx'],
 }
 
 # Create the custom 404.html file with the message
 file { '/var/www/html/404.html':
   ensure  => 'file',
   content => "Ceci n'est pas une page",
+  require => Package['nginx'],
 }
 
 # Ensure Nginx is running and enabled at boot
 service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
+  ensure    => 'running',
+  enable    => true,
+  require   => [
+    File['/etc/nginx/sites-available/custom_header'],
+    File['/var/www/html/index.nginx-debian.html'],
+    File['/var/www/html/404.html'],
+  ],
 }
+
