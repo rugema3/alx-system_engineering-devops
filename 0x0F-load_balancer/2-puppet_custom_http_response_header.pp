0177b54969610be6a8db683d4da34ff682c9a354
rugema3 <rugema61@gmail.com>
2023-10-07 12:02:36 +0200

    updating the puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,66 +1,26 @@
 # Install Nginx
-exec { 'update':
-  command => 'sudo apt-get -y update',
-  path    => ['/usr/bin', '/usr/sbin'],
+
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['update'],
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
 
-# Create a custom Nginx configuration file
-file { '/etc/nginx/sites-available/custom_header':
-  ensure  => 'file',
-  content => "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By \$hostname;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location = /404.html {
-      root /var/www/html;
-      internal;
-    }
-}",
-  require => Package['nginx'],
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
 
-# Create a symbolic link to enable the custom configuration
-file { '/etc/nginx/sites-enabled/custom_header':
-  ensure  => 'link',
-  target  => '/etc/nginx/sites-available/custom_header',
-  require => File['/etc/nginx/sites-available/custom_header'],
-}
-
-# Create the default HTML file with your content
-file { '/var/www/html/index.nginx-debian.html':
-  ensure  => 'file',
-  content => 'Hello World!',
-  require => Package['nginx'],
-}
-
-# Create the custom 404.html file with the message
-file { '/var/www/html/404.html':
-  ensure  => 'file',
-  content => "Ceci n'est pas une page",
-  require => Package['nginx'],
-}
-
-# Ensure Nginx is running and enabled at boot
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  require   => [
-    File['/etc/nginx/sites-available/custom_header'],
-    File['/var/www/html/index.nginx-debian.html'],
-    File['/var/www/html/404.html'],
-  ],
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
 
