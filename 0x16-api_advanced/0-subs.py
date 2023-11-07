7f7032017802f2a27f7552d50f7d1cda8111894e
rugema3 <rugema61@gmail.com>
2023-11-07 21:51:48 +0200

    a function that returns the number of subscribers given a certain topic.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,16 +5,17 @@ import requests
 # Reddit API credentials
 user_agent = 'rugema3/1.0'
 
+
 def number_of_subscribers(subreddit):
     """
-    Query the Reddit API and return the number of subscribers 
+    Query the Reddit API and return the number of subscribers
     for a given subreddit.
 
     Args:
         subreddit (str): The name of the subreddit to query.
 
     Returns:
-        int: The number of subscribers for the subreddit, 
+        int: The number of subscribers for the subreddit,
         or 0 if the subreddit is invalid or an error occurs.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
