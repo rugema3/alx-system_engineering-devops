7321c6e8959224a195be40b73cebdb19f9723b2d
rugema3 <rugema61@gmail.com>
2023-11-07 22:04:11 +0200

    a function that will query the top ten topics.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Return the top 10 hot topic."""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Query the Reddit API and print the titles of the first
+    10 hot posts for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        None
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {
+        "User-Agent": "rugema3/1.0"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        # Check if the subreddit is invalid (HTTP status code 404)
+        if response.status_code == 404:
+            print("None")
+            return
+
+        data = response.json()
+        posts = data['data']['children']
+
+        if not posts:
+            print("No hot posts found in this subreddit.")
+            return
+
+        for i, post in enumerate(posts):
+            print(f"{post['data']['title']}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
