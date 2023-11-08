20e41add70a150616de7897ada76640ea31ab0fb
rugema3 <rugema61@gmail.com>
2023-11-08 22:22:17 +0200

    adding comments to the puppet file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Using strace to diagnose what's wrong with the server. and configure puppet
 exec {'replace':
   provider => shell,
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'}
