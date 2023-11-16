24ff7d032937c07aeb4878592fbc3ff2fad35d02
rugema3 <rugema61@gmail.com>
2023-11-16 18:39:16 +0000

    increasing user limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+exec { 'adjust-hard-limits-for-holberton':
+  command => "sed -i '/^holberton hard/s/4/50000/g' /etc/security/limits.conf",
+  path    => '/usr/local/bin:/bin/'
+}
+
+exec { 'adjust-soft-limits-for-holberton':
+  command => "sed -i '/^holberton soft/s/5/50000/g' /etc/security/limits.conf",
+  path    => '/usr/local/bin:/bin/'
+}
+
