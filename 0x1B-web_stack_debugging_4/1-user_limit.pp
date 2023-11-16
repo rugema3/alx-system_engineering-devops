daee22426d43675bf9622dbb5df0cfec689af135
rugema3 <rugema61@gmail.com>
2023-11-16 18:47:20 +0000

    adding a comment to explain what the puppet does

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,5 @@
+# Adjust user limits.
+
 exec { 'adjust-hard-limits-for-holberton':
   command => "sed -i '/^holberton hard/s/4/50000/g' /etc/security/limits.conf",
   path    => '/usr/local/bin:/bin/'
